4a41a6de606a1f0788a47fa4b772f62ee1b5ef52
Mark Wilkinson <markw@illuminae.com>
2025-02-24 20:48:34 +0100

    misspelled trails

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,15 +2,14 @@
 # ENV['GRAPHDB_PORT'] = '443'
 # ENV['GRAPHDB_REPONAME'] = 'fdpindex-fdp'
 
-require 'rack/test'
-require 'rspec/openapi'
-require 'pry'
-require_relative '../app/controllers/application_controller' # Adjust the path to your Sinatra app file
+require "rack/test"
+require "rspec/openapi"
+require "pry"
+require_relative "../app/controllers/application_controller" # Adjust the path to your Sinatra app file
 
 OPENAPI = 1
-RSpec::OpenAPI.path = 'doc/schema.yaml'
-RSpec::OpenAPI.servers = [{ url: 'https://tools.ostails.eu' }]
-
+RSpec::OpenAPI.path = "doc/schema.yaml"
+RSpec::OpenAPI.servers = [{ url: "https://tools.ostrails.eu" }]
 
 RSpec.describe ChampionApp, type: :request do
   include Rack::Test::Methods
@@ -20,10 +19,10 @@ RSpec.describe ChampionApp, type: :request do
     ChampionApp.new
   end
 
-  describe 'GET /champion/sets/' do
-    it 'returns a list of known sets' do
-      header 'accept', 'application/json'
-      get '/champion/sets/'
+  describe "GET /champion/sets/" do
+    it "returns a list of known sets" do
+      header "accept", "application/json"
+      get "/champion/sets/"
 
       # puts "Status: #{last_response.status}"          # e.g., 200
       puts "Body: #{last_response.body}" # e.g., '{"message":"Hello, world!"}'
@@ -38,19 +37,19 @@ RSpec.describe ChampionApp, type: :request do
         # "description": "Tests covering all Principles, mirroring the tests that were available from the FAIR Evaluator",
         # "creator": "mark.wilkinson@upm.es",
         # "tests": [
-        expect(set['identifier'].class).to eq String
-        expect(set['title'].class).to eq String
-        expect(set['description'].class).to eq String
-        expect(set['creator'].class).to eq String
-        expect(set['tests'].class).to eq Array
+        expect(set["identifier"].class).to eq String
+        expect(set["title"].class).to eq String
+        expect(set["description"].class).to eq String
+        expect(set["creator"].class).to eq String
+        expect(set["tests"].class).to eq Array
       end
     end
   end
 
-  describe 'GET /champion/tests/' do
-    it 'returns a list of known tests' do
-      header 'accept', 'application/json'
-      get '/champion/tests/'
+  describe "GET /champion/tests/" do
+    it "returns a list of known tests" do
+      header "accept", "application/json"
+      get "/champion/tests/"
 
       # puts "Status: #{last_response.status}"          # e.g., 200
       expect(last_response).to be_ok
@@ -65,9 +64,9 @@ RSpec.describe ChampionApp, type: :request do
       json = JSON.parse(last_response.body).first
       json.keys.first do |_context|
         testt = json[testid]
-        expect(testt['api'].class).to eq String
-        expect(testt['title'].class).to eq String
-        expect(testt['description'].class).to eq String
+        expect(testt["api"].class).to eq String
+        expect(testt["title"].class).to eq String
+        expect(testt["description"].class).to eq String
       end
     end
   end
